cc70c1c70c9d364ddf0a718bb1317fcc67c398b2
오송주 <dhthdwn7920@gmail.com>
2025-07-15 01:01:18 +0900

    setting: spring boot setting

---
+++ src/main/java/org/example/yeye_backend/YeyeBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.yeye_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeyeBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeyeBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/yeye_backend/YeyeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.yeye_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeyeBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
